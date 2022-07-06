c113efcbd547a536c5ab87af33523aea1dd5ec39
icemoon97 <julianbaldwin97@gmail.com>
2022-07-06 15:14:11 -0700

    possible fix?

--- app.py
+++ app.py
@@ -23,11 +23,10 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/", methods=("GET", "POST"))
 def index():
-    return send_from_directory(app.root_path + '/react-client/build', 'index.html')
+    return app.send_static_file('index.html')
 
 initialize_routes(api)
 
-# enables flask app to run using "python3 app.py"
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
 
